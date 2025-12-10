9b9d94dd7207da0ed8b91041d895cbb60aaf4d45
gauravvermagkp <69907581+gauravvermagkp@users.noreply.github.com>
2025-12-11 00:38:37 +0530

    batchtiming_css_v2_working

--- src/components/BatchTimings.jsx
+++ src/components/BatchTimings.jsx
@@ -22,12 +22,12 @@ export default function BatchTimings({ region }) {
                         const newt = convertedTime2(start_end_time, preferredBase)                  
                         return <div className={`batch-row batch-${batchname.toLowerCase()}`}>
                             <label>{`${batchname} : `}</label>
-                            <button className="btn-soft" id={`${batchname}b1`} value={newt[0]} onClick={(e)=>{
+                            <button className="btn-soft" id={`${batchname.toLowerCase()}b1`} value={newt[0]} onClick={(e)=>{
                                 setbid(`${batchname}b1`)
                                 dispatch(updateInputDate(e.target.value))
                                 }}>{newt[0]}</button>
                             <label> -- </label>                            
-                            <button className="btn-soft" id={`${batchname}b2`} value={newt[1]} onClick={(e)=>{
+                            <button className="btn-soft" id={`${batchname.toLowerCase()}b2`} value={newt[1]} onClick={(e)=>{
                                 setbid(`${batchname}b2`)
                                 dispatch(updateInputDate(e.target.value))
                                 }}>{newt[1]}</button>
