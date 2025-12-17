c7a171c9628080ef2550b9dd60512a08088a99e8
gauravvermagkp <69907581+gauravvermagkp@users.noreply.github.com>
2025-12-17 11:10:54 +0530

    Refactor button click handler for collapse button

--- src/components/UserInputSection.jsx
+++ src/components/UserInputSection.jsx
@@ -59,12 +59,14 @@ export default function UserInputSection({ props_object }) {
         <>
             <div className='card'>
                 <h3>User Input Converted</h3>
-                <button type="button" class="collapse-btn" onClick={() => dispatch(updatecollapseConverted(!collapseConverted))}>{collapseConverted ? '+' : '-'}</button>
+                <button type="button" class="collapse-btn" onClick={() => {
+            dispatch(updatecollapseConverted(!collapseConverted))}>{collapseConverted ? '+' : '-'
+                }}</button>
                 {!collapseConverted ? <>
                     <label className='cardlabel'>Choose Date & Time ({preferredBase}):</label>
                     <input id="userInput" type="datetime-local" value={inputDate} onChange={(e) => {
                         dispatch(updateInputDate(e.target.value))
-                        dispatch(updateClickedclassName(''))
+                        dispatch(updateClickedRegion(''))
                     }} />                   
 
                     <div className="prominent" >
